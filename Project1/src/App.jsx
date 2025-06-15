400ca39599f1b299ddb7e863d68f58d0394597f8
rajyadav63 <vector36razz@gmail.com>
2025-06-15 16:32:47 +0530

    Navbar added

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div className='bg-blue-400'>
-      React Projects
+    <div>
+      <Navbar />
     </div>
   )
 }
--- Project1/src/components/Navbar.jsx
+++ Project1/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='flex justify-around align-middle text-center mt-5'>
+            <div>
+                <img src="/brand_logo.png" alt="logo" />
+            </div>
+            <div>
+                <ul className='flex justify-center gap-5'>
+                    <li> <a href="#"></a>MENU</li>
+                    <li> <a href="#"></a>LOCATION</li>
+                    <li> <a href="#"></a>ABOUT</li>
+                    <li> <a href="#"></a>CONTACT</li>
+                </ul>
+            </div>
+            <div className='bg-red-700 text-center text-white h-[25px] w-[90px] rounded'>
+                <button className='text-center'>Login</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
